3d64e3d255ca09a44c8a4fb72b1987b99ba2a484
Samarth Sharma <samarth.sharma@in.ncs-i.com>
2024-12-09 17:58:19 +0530

    working discovery service

--- backend/discovery-service/src/main/java/expense/discovery_service/DiscoveryApplication.java
+++ backend/discovery-service/src/main/java/expense/discovery_service/DiscoveryApplication.java
@@ -2,8 +2,10 @@ package expense.discovery_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryApplication {
 
 	public static void main(String[] args) {
